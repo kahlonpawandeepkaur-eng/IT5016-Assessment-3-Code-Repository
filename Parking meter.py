29307d737494a2e743dfcb490f31a8888d379639
kahlonpawandeepkaur-eng <kahlonpawandeepkaur@gmail.com>
2026-09-08 10:57:09 +1200

    Add files via upload

--- Parking meter.py
+++ Parking meter.py
@@ -7,57 +7,57 @@ Parking Meter System
 """
 
 # ==========================================
-# PART 1: Parking Meter System
+# PART 1: Parking Meter Process
 # ==========================================
 
-# Initialize the ticket counter
+# Initialize a global ticket counter
 ticket_counter = 10000
 
 def parking_meter():
     global ticket_counter
 
 
-    # Display a message to show the start of the parking meter process
+    # 1. Display a message with standard decorations to indicate the start of the parking meter process
     print("_" * 30)
-    print("Kia Ora! This is the parking meter")
+    print("Kia Ora! This is parking meter")
     print("_" * 30)
 
-    # Set the parking time to 4 hour
+    # 2. Creates and sets the value for variable ParkTime = 4 hours
     park_time = 4
 
-    # Set the initial rate and cost
+    # 3. Creates and sets value for variables: rate and cost
     rate = 4
     cost = 0
 
-    # Calculates the parking cost using an IF statement
+    # 4. Calculates the parking charges for parktime using an IF statement
     if park_time > 3:
         cost = rate * 3
 
-        # Reduce the rate by $2
+        # Drop the rate by $2
         rate -= 2
 
         remaining_time = park_time - 3
 
-        # Add the remaining time cost
+        # Add to the current cost
         cost += rate * remaining_time 
     else:
         cost = rate * park_time
 
-    # Create a unique ticket ID by incrementing the counter
+    # Generate a unique ticket ID by incrementing the counter
     ticket_counter += 1
     ticket_id = ticket_counter    
 
-    # Return the parking details and ticket ID as a tuple
+    # Return the parking variables and ticket ID as a tuple
     return park_time, cost, ticket_id
 
 # ==================================================
 # PART 2: Testing the Parking Meter Function
 #===================================================
 
-# Call the function and unpack the returned values
+# Call the function to process parking and unpack the returned values
 park_time, cost, ticket_id = parking_meter()
 
-# 5. Display the calculated parking charges 
+# 5. Display appropriate message showing the calculated parking charges 
 print()
 print("_" * 30)
 print("\nPrinting Parking Meter Details:")
